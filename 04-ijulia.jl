f6b66bf5f18b8d138476b70889e79bdaf2cdd046
laj <larry.jones@reveal-energy.com>
2021-10-15 09:27:22 -0500

    Using Julia with Jupyter notebooks

---
+++ 04-ijulia.jl
@@ -0,0 +1,22 @@
+# Installing and working with IJulia
+
+println("IJulia")
+println("======")
+println()
+println("""The `IJulia` package supports using Jupyter notebooks with a
+Julia backend.\n
+
+To use `IJulia`, begin by adding the `IJulia` package to the 
+current environment.\n
+
+After adding the `IJulia` package, execute `using IJulia`.\n
+
+Finaly, after adding the `IJulia` package, execute the function,
+`notebook()`.\n
+
+After executing the `notebook()` function, Julia will prompt you 
+to install the Jupyter notebook using `conda`.\n
+
+Once Julia installs the Jupyter notebook using `conda`, Jupyter
+will add a new tab to the browser with the Jupyter notebook.
+""")
