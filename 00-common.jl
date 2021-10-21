8753e4a5ecd3349d0630a4fb408f977bf7ab85fc
laj <larry.jones@reveal-energy.com>
2021-10-21 10:03:20 -0500

    Print expressions

--- 00-common.jl
+++ 00-common.jl
@@ -11,3 +11,14 @@ function print_details(text::AbstractString)
     println()
     println(text)
 end
+
+function print_expression(text::AbstractString, expression::Bool)
+    println("$(text) is $(expression)")
+end
+
+function print_expression(text::AbstractString, expression::Any, comment::AbstractString="")
+    println("$(text) == $(expression)")
+    if length(comment) > 0
+        println("$(comment)")
+    end
+end
